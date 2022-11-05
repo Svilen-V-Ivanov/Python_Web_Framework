d5aed950efe409b3f2929e3294b35e9a553b0fbb
TheShadowTM <svilo_007@abv.bg>
2022-11-05 18:25:16 +0200

    Removing all but necessary

--- forms_validations/forms_validations/web/models_validators.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-
-from forms_validations.web.models import Todo
-
-
-def validate_max_todos_per_person(assignee):
-    if assignee.todo_set.count() >= Todo.MAX_TODOS_PER_PERSON:
-        raise ValidationError(f"{assignee} already has max TODOs assigned.")
